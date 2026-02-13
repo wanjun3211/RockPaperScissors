98e0754659d5a7becc33bd209e40f4c66843484f
Vic <123456wangtom@gmail.com>
2026-02-13 20:50:45 +1300

    modified:   javascript.js
    Create function of computer generation result

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("test");
+function getComputerChoice() {
+  const randomNum = Math.random();
+
+  if (randomNum >= 0 && randomNum < (1/3)) {
+    return "rock";
+  }
+  else if (randomNum >= 1/3 && randomNum < (2/3)) {
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+}
+
+
