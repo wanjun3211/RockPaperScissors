7d5c83ef68255241b10a506f7a9d0f454e38f4d9
Vic <123456wangtom@gmail.com>
2026-02-14 10:18:53 +1300

    modified:   javascript.js
    Modify getHumanchoice function, and let user input
    those three choice explicitly instead of number

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
+let humanScore = 0, computerScore = 0;
+
+
 function getComputerChoice() {
   const randomNum = Math.random();
 
-  if (randomNum >= 0 && randomNum < (1/3)) {
+  if (randomNum >= 0 && randomNum < (1 / 3)) {
     return "rock";
   }
-  else if (randomNum >= 1/3 && randomNum < (2/3)) {
+  else if (randomNum >= 1 / 3 && randomNum < (2 / 3)) {
     return "paper";
   }
   else {
@@ -12,4 +15,26 @@ function getComputerChoice() {
   }
 }
 
+
+function getHumanChoice() {
+  const userInput = prompt("Please input your choice: rock, scissors, or paper", "");
+
+  const modify_Input = userInput.toLowerCase();
+
+  switch (modify_Input) {
+
+    case "rock":
+      return "rock";
+
+    case "scissors":
+      return "scissors";
+
+    case "paper":
+      return "paper";
+
+    default:
+      return 'User input is not correct. Should be rock, scissors, or paper'
+  }
+
+}
 
