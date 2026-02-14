08d335a99d370590a75fc76b1b6c32041586c8e1
Vic <123456wangtom@gmail.com>
2026-02-14 14:16:04 +1300

    modified:   javascript.js
    Creat the playAround function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,5 @@
 let humanScore = 0, computerScore = 0;
 
-
 function getComputerChoice() {
   const randomNum = Math.random();
 
@@ -19,9 +18,7 @@ function getComputerChoice() {
 function getHumanChoice() {
   const userInput = prompt("Please input your choice: rock, scissors, or paper", "");
 
-  const modify_Input = userInput.toLowerCase();
-
-  switch (modify_Input) {
+  switch (userInput) {
 
     case "rock":
       return "rock";
@@ -38,3 +35,61 @@ function getHumanChoice() {
 
 }
 
+function playRound(humanChoice, computerChoice) {
+
+  const modify_humanChoice = humanChoice.toLowerCase();
+
+  if (modify_humanChoice === computerChoice) {
+    console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, and it is a tie`);
+  } else {
+
+    if (modify_humanChoice === 'rock' && computerChoice === 'paper') {
+      console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, you lose as paper beats rock`);
+
+      ++computerScore;
+    }
+
+    if (modify_humanChoice === 'rock' && computerChoice === 'scissors') {
+      console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, you win as rock beats scissors`);
+
+      ++humanScore;
+    }
+
+    if (modify_humanChoice === 'scissors' && computerChoice === 'paper') {
+      console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, you win as scissors beats paper`);
+
+      ++humanScore;
+    }
+
+    if (modify_humanChoice === 'scissors' && computerChoice === 'rock') {
+      console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, you lose as rock beats scissors`);
+
+      ++computerScore;
+    }
+
+
+    if (modify_humanChoice === 'paper' && computerChoice === 'rock') {
+      console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, you win as paper beats rock`);
+
+      ++humanScore;
+    }
+
+
+     if (modify_humanChoice === 'paper' && computerChoice === 'scissors') {
+      console.log(`You chose ${humanChoice} 
+      while computer opted ${computerChoice}, you lose as scissors beats paper`);
+
+      ++computerScore;
+    }
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
